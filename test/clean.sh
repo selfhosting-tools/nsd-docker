9d1e38de8263e7a7bf36b1817d63b322afb8fbe7
FL42 <46161216+fl42@users.noreply.github.com>
2019-11-23 12:48:45 +0100

    fix: docker rmi

--- test/clean.sh
+++ test/clean.sh
@@ -4,4 +4,4 @@ set -e
 
 docker container stop nsd_unsigned nsd_default || true
 docker container rm --volumes nsd_unsigned nsd_default || true
-docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker 
+docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi
