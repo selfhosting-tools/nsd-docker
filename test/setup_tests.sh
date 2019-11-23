dbbdfa01a3c83459a8958ad271e49164d7668429
FL42 <46161216+fl42@users.noreply.github.com>
2019-11-23 12:45:19 +0100

    fix: undefined variable

--- test/setup_tests.sh
+++ test/setup_tests.sh
@@ -11,14 +11,16 @@ docker run \
     --name nsd_unsigned \
     -v "`pwd`/test/config/nsd.conf":/etc/nsd/nsd.conf \
     -v "`pwd`/test/config/db.example.org":/zones/db.example.org \
-    -t $(NAME)
+    -t nsd-docker:latest
 
 docker run \
     -d \
     --name nsd_default \
     -v "`pwd`/test/config/nsd.conf":/etc/nsd/nsd.conf \
     -v "`pwd`/test/config/db.example.org":/zones/db.example.org \
-    -t $(NAME)
+    -t nsd-docker:latest
+
+sleep 2
 
 # Set up DNSSEC-signed zone
 docker exec nsd_default keygen example.org
